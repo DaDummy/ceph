cc68a267c59af7968181dc3e29533e79821817dd
Christoffer Anselm <c.anselm@paindevs.com>
2019-10-01 12:24:43 +0200

    install-deps.sh: Added missing $SUDO for Ubuntu Disco-specific apt-get call
    install-deps.sh was failing on Ubuntu Disco when run as non-root. This change fixes the issue.
    
    Signed-Off-by: c.anselm@paindevs.com <Christoffer Anselm>

--- install-deps.sh
+++ install-deps.sh
@@ -315,7 +315,7 @@ else
                 [ ! $NO_BOOST_PKGS ] && install_boost_on_ubuntu bionic
                 ;;
             *Disco*)
-                [ ! $NO_BOOST_PKGS ] && apt-get install -y libboost1.67-all-dev
+                [ ! $NO_BOOST_PKGS ] && $SUDO apt-get install -y libboost1.67-all-dev
                 ;;
             *)
                 $SUDO apt-get install -y gcc
